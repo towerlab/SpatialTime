7d2fb4283f176e8eeb503578ab09043935f39fd4
dimitrisokolowskei <dimitrisokolowskei@gmail.com>
2025-01-29 10:36:16 -0600

    script update

--- R/SpatialTime.R
+++ R/SpatialTime.R
@@ -9,20 +9,24 @@
 #' @import tidyverse
 #' @export
 
-SpatialTime <- function(file = NULL, id = NULL) {
+SpatialTime <- function(file = NULL, reference = NULL, compare = NULL) {
 
-  if (!all(c("row","col","imagerow","imagecol") %in% names(file))) {
+  if (is.null(file)) {
+    stop("Input data not found.")
+  }
+
+  if (!all(c("row","col","imagerow","imagecol") %in% names(compare))) {
     stop("Is this a coordinate file?")
   }
 
-  tissue_to <- file
+  tissue_to <- compare
   tissue_to <- tissue_to %>%
     mutate(barcode = X) %>%
     select(imagerow, imagecol, barcode)
 
-  if (!is.null(id)) {
-    tissue_from <- data %>%
-      select(contains(id)) %>%
+  if (!is.null(reference)) {
+    tissue_from <- file %>%
+      select(contains(reference)) %>%
       filter_if(is.numeric, all_vars((.) != 0))
 
   } else {
@@ -31,7 +35,7 @@ SpatialTime <- function(file = NULL, id = NULL) {
 
   st_calc <- tissue_to %>%
     rowwise() %>%
-    mutate(st_abs = min(sqrt((imagerow - tissue_from[[paste0(id, "_row")]]) ^ 2 + (imagecol - tissue_from[[paste0(id, "_col")]]) ^ 2))) %>%
+    mutate(st_abs = min(sqrt((imagerow - tissue_from[[paste0(reference, "_row")]]) ^ 2 + (imagecol - tissue_from[[paste0(reference, "_col")]]) ^ 2))) %>%
     ungroup() %>%
     mutate(st_rel = (st_abs - min(st_abs)) / (max(st_abs) - min(st_abs)))
 

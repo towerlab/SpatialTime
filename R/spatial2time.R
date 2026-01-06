8e735fce7155d239260ebd79648451f97f6d765f
dimitrisokolowskei <dimitrisokolowskei@gmail.com>
2026-01-06 09:07:43 -0600

    spatial2time fix

--- R/spatial2time.R
+++ R/spatial2time.R
@@ -40,6 +40,52 @@ Spatial2Time <- function(file = NULL, file2 = NULL, id = NULL) {
   return(st_calc)
 }
 
+
+
+#' Spatial2TimeHD
+#' @param file Seurat's cluster coordinates as input
+#' @param file2 Seurat's reference cluster coordinates as input
+#' @param id Export coordinates files
+#'
+#' @details
+#' This function calculates and adds coordinates values to each line drawn in data frame.
+#'
+#' @import Seurat
+#' @import tidyverse
+#' @export
+
+Spatial2TimeHD <- function(file = NULL, file2 = NULL, id = NULL) {
+
+  columns <- c("x","y")
+
+  for (i in c(file, file2)) {
+
+    if (!all(columns %in% names(i))) {
+    } else {
+
+      stop("Is this a coordinate file?")
+    }
+  }
+
+  tissue_to <- file %>%
+    mutate(barcode = ...1) %>%
+    select(x, y, barcode)
+
+  tissue_from <- file2 %>%
+    mutate(barcode = ...1) %>%
+    select(x, y, barcode)
+
+  st_calc <- tissue_to %>%
+    rowwise() %>%
+    mutate(st_abs = min(sqrt((x - tissue_from[["x"]]) ^ 2 + (y - tissue_from[["y"]]) ^ 2))) %>%
+    ungroup() %>%
+    mutate(st_rel = (st_abs - min(st_abs)) / (max(st_abs) - min(st_abs)))
+
+  return(st_calc)
+}
+
+
+
 #' SpatialShinyTime
 #' @param file Seurat's cluster coordinates as input
 #' @param file2 Seurat coordinates of reference spots selected in ShinyApp
